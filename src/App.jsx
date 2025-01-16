a07d2811ebab2448d5d4aaf538c4f6cde2411a32
ryan <laslomcd@gmail.com>
2025-01-16 11:57:11 -0500

    added image and accordion sections

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,24 @@ import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Companies from "./components/Companies/Companies";
 import Residences from "./components/Residences/Residences";
-import './App.css'
+import "./App.css";
 import Value from "./components/Value/Value";
+import Contact from "./components/Contact/Contact";
 
 function App() {
-  return (
-    <div className="App">
-      <div>
-        <div className="white-gradient" />
-        <Header />
-        <Hero />
-      </div>
-      <Companies />
-      <Residences />
-      <Value />
-    </div>
-  );
+	return (
+		<div className="App">
+			<div>
+				<div className="white-gradient" />
+				<Header />
+				<Hero />
+			</div>
+			<Companies />
+			<Residences />
+			<Value />
+			<Contact />
+		</div>
+	);
 }
 
 export default App;
