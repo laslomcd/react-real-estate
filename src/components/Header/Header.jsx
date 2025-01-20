cbd8ab2a7ea358c269db2993037ae22f9e03a26d
ryan <laslomcd@gmail.com>
2025-01-20 16:40:21 -0500

    worked on mobile responsiveness

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,40 @@
-import './Header.css'
+import React, { useState } from "react";
+import "./Header.css";
+import { BiMenuAltRight } from "react-icons/bi";
+import OutsideClickHandler from "react-outside-click-handler";
 
 const Header = () => {
-  return (
-      <section className='h-wrapper'>
-          <div className='flexCenter paddings innerWidth h-container'>
-              <img src="./logo.png" alt="" width={100} />
-              <div className="flexCenter h-menu">
-                  <a href="">Residencies</a>
-                  <a href="">Our Value</a>
-                  <a href="">Contact Us</a>
-                  <a href="">Get Started</a>
-                  <button className='button'>
-                      <a href="">Contact</a>
-                  </button>
-                  
-              </div>
-          </div>
-    </section>
-  )
-}
+	const [menuOpen, setMenuOpen] = useState(false);
 
-export default Header
+	const getMenuStyles = (menuOpen) => {
+		if (document.documentElement.clientWidth <= 800) {
+			return {
+				right: !menuOpen && "-100%",
+			};
+		}
+	};
+	return (
+		<section className="h-wrapper">
+			<div className="flexCenter paddings innerWidth h-container">
+				<img src="./logo.png" alt="" width={100} />
+				<OutsideClickHandler onOutsideClick={() => setMenuOpen(false)}>
+					<div className="flexCenter h-menu" style={getMenuStyles(menuOpen)}>
+						<a href="">Residencies</a>
+						<a href="">Our Value</a>
+						<a href="">Contact Us</a>
+						<a href="">Get Started</a>
+						<button className="button">
+							<a href="">Contact</a>
+						</button>
+					</div>
+				</OutsideClickHandler>
+
+				<div className="menu-icon" onClick={() => setMenuOpen((prev) => !prev)}>
+					<BiMenuAltRight size={30} />
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default Header;
